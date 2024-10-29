af0b07f5d9c8b95e52649908773a168c34b690d3
jcaitou <71485747+jcaitou@users.noreply.github.com>
2024-10-28 22:30:43 -0700

    add vercel

--- astro.config.ts
+++ astro.config.ts
@@ -15,6 +15,8 @@ import astrowind from './vendor/integration';
 
 import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehypePlugin } from './src/utils/frontmatter';
 
+import vercel from '@astrojs/vercel/serverless';
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const hasExternalScripts = false;
@@ -89,4 +91,6 @@ export default defineConfig({
       },
     },
   },
+
+  adapter: vercel(),
 });
